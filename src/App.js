629f594e380c8443c3c2afc4f87cabdd4db8b44c
Ludo <cary.ludo@te.com>
2020-07-01 22:35:32 +0800

    项目初始化

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>你好</h1>
+      <style jsx>
+        {`h1 {
+          color:red;
+        }`}
+      </style>
+  </div>
+  );
+}
+
+export default App;
